5656c2c7f11cbb6d7ececa50b70dc2f8b65f83d8
LOFTERART <439017006@qq.com>
2020-08-25 19:37:20 +0800

    fix: init

--- cmd/cobra.go
+++ cmd/cobra.go
@@ -2,13 +2,11 @@ package cmd
 
 import (
 	"errors"
-	"fmt"
 	"github.com/spf13/cobra"
 	"go-admin/cmd/api"
 	"go-admin/cmd/config"
 	"go-admin/cmd/migrate"
 	"go-admin/cmd/version"
-	"go-admin/global"
 	"go-admin/tools"
 	"os"
 )
@@ -20,22 +18,13 @@ var rootCmd = &cobra.Command{
 	Long:         `go-admin`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
-			tip()
 			return errors.New(tools.Red("requires at least one arg"))
 		}
 		return nil
 	},
 	PersistentPreRunE: func(*cobra.Command, []string) error { return nil },
 	Run: func(cmd *cobra.Command, args []string) {
-		tip()
 	},
-}
-
-func tip() {
-	usageStr := `欢迎使用 ` + tools.Green(`go-admin `+global.Version) + ` 可以使用 ` + tools.Red(`-h`) + ` 查看命令`
-	usageStr1 := `也可以参考 http://doc.zhangwj.com/go-admin-site/guide/ksks.html 里边的【启动】章节`
-	fmt.Printf("%s\n", usageStr)
-	fmt.Printf("%s\n", usageStr1)
 }
 
 func init() {
